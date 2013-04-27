0964254472c24883efa22e0bdbac68ed951b0130
John Ihlein <jihlein@hotmail.com>
2013-04-26 21:54:04 -0600

    Config changes for GNU ARN compiler

--- src/system_stm32f4xx.c
+++ src/system_stm32f4xx.c
@@ -209,9 +209,9 @@ void SystemInit(void)
 {
   /* FPU settings ------------------------------------------------------------*/
 
-  //#if (__FPU_PRESENT == 1) && (__FPU_USED == 1)                                      // HJI - Comment out line
+  #if (__FPU_PRESENT == 1) && (__FPU_USED == 1)
     SCB->CPACR |= ((3UL << 10*2)|(3UL << 11*2));  /* set CP10 and CP11 Full Access */
-  //#endif                                                                             // HJI - Comment out line
+  #endif
 
   /* Reset the RCC clock configuration to the default reset state ------------*/
   /* Set HSION bit */
